78b73696a93b5cbeb92344f4bffb2f9c33fcdf41
KristjanJakob <kja15@hi.is>
2026-09-03 10:20:57 +0000

    Add Spring Boot project skeleton

---
+++ src/main/java/is/hi/team6/newsaggregator/NewsaggregatorApplication.java
@@ -0,0 +1,13 @@
+package is.hi.team6.newsaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsaggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsaggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/is/hi/team6/newsaggregator/NewsaggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.team6.newsaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsaggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
